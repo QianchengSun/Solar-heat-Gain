8fe75a093a377b041c630a3cfe1c04dd4ab3d96d
Qiancheng Sun <qianchengsun@Qianchengs-MacBook-Pro.local>
2021-10-29 16:17:31 -0400

    calculationn

---
+++ solar_heat_gain.py
@@ -0,0 +1,22 @@
+#%% programming about solar heat gain calculation based on LSTM-GRU
+
+import numpy as np
+import pandas as pd
+import math 
+from math import pi
+import sys
+package_path = r"/Users/qianchengsun/PhD/solar_heat_gain/python_code/solar_calculation.py"
+sys.path.append(package_path)
+import solar_calculation as sc
+
+#%%
+
+
+
+
+
+
+
+# %%
+beam = sc.Beam_solar_radiation_east(month=)
+# %%
